0b521148b6eb9b767f4914c40cb3daa3d82f6168
Jakob Rößler <roessler@sighalt.de>
2018-09-20 17:25:12 +0200

    made log_exception decorator importable from logdecorator

--- logdecorator/__init__.py
+++ logdecorator/__init__.py
@@ -1,3 +1,3 @@
-from .decorator import log_on_start, log_on_error, log_on_end
+from .decorator import log_on_start, log_on_error, log_on_end, log_exception
 
-__all__ = ["log_on_start", "log_on_error", "log_on_end"]
+__all__ = ["log_on_start", "log_on_error", "log_on_end", "log_exception"]
